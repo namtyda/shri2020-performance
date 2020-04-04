23b71855e5cb9fb2d9bf442008f54f7e43ca4061
Andrey Pogorelov <namtydapro@gmail.com>
2020-04-04 23:32:00 +0300

    observer

--- js/main2.js
+++ js/main2.js
@@ -33,4 +33,20 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
     })
+    const target = document.querySelector('.critical-cam');
+
+    const obs = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let div = entry.target
+                div.style.background = `url('${entry.target.getAttribute('data-src')}')`;
+                div.style.backgroundSize = '100%'
+                entry.target.removeAttribute('data-src');
+                obs.unobserve(div)
+            }
+        });
+
+    });
+    obs.observe(target);
+
 }, !1);
